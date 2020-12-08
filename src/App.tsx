5fde8be6f68029df726fe3272114adda27440e83
Lucas <lucasmercado101@gmail.com>
2020-12-08 12:41:05 -0300

    made skeletons use theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,6 @@ import React from "react";
 import Films from "./pages/Films";
 import axios from "axios";
 
-/*
-#222831
-#30475e
-#f2a365
-#ececec
-*/
-
 axios.defaults.baseURL = "https://ghibliapi.herokuapp.com";
 // axios.defaults.baseURL =
 //   "https://cors-anywhere.herokuapp.com/https://ghibliapi.herokuapp.com";
--- src/components/Card/Species.tsx
+++ src/components/Card/Species.tsx
@@ -1,40 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "react-query";
 import { fetchUrls } from "../../Api";
 import ContentLoader from "react-content-loader";
 import Dropdown from "./Dropdown/Dropdown";
 import DropdownContent from "./Dropdown/Content";
-import { ClassificationType } from "typescript";
-import styled from "styled-components";
+import styled, { ThemeContext } from "styled-components";
 
-type Props = {
-  species: string[];
+const MyLoader: React.FC = () => {
+  const { primary, accent } = useContext(ThemeContext);
+  return (
+    <ContentLoader
+      speed={2}
+      width={400}
+      height={120}
+      viewBox="0 0 400 120"
+      backgroundColor={primary}
+      foregroundColor={accent}
+    >
+      <rect x="0" y="13" width="20" height="5" />
+      <rect x="30" y="10" rx="5" ry="5" width="200" height="10" />
+      <rect x="0" y="43" width="20" height="5" />
+      <rect x="30" y="40" rx="5" ry="5" width="200" height="10" />
+      <rect x="0" y="73" width="20" height="5" />
+      <rect x="30" y="70" rx="5" ry="5" width="200" height="10" />
+      <rect x="0" y="103" width="20" height="5" />
+      <rect x="30" y="100" rx="5" ry="5" width="200" height="10" />
+    </ContentLoader>
+  );
 };
-
-const MyLoader = () => (
-  <ContentLoader
-    speed={2}
-    width={400}
-    height={120}
-    viewBox="0 0 400 120"
-    backgroundColor="white"
-    foregroundColor="#f2a365"
-  >
-    <rect x="0" y="13" width="20" height="5" />
-    <rect x="30" y="10" rx="5" ry="5" width="200" height="10" />
-    <rect x="0" y="43" width="20" height="5" />
-    <rect x="30" y="40" rx="5" ry="5" width="200" height="10" />
-    <rect x="0" y="73" width="20" height="5" />
-    <rect x="30" y="70" rx="5" ry="5" width="200" height="10" />
-    <rect x="0" y="103" width="20" height="5" />
-    <rect x="30" y="100" rx="5" ry="5" width="200" height="10" />
-  </ContentLoader>
-);
 
 const Classification = styled.div`
   color: ${({ theme }) => theme.accentDark};
   display: inline;
 `;
+
+type Props = {
+  species: string[];
+};
 
 const Species: React.FC<Props> = ({ species }) => {
   const { data, refetch, isFetched } = useQuery(
